3e868183b9af932626152bbbe582ec8ee9a8c3f9
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2025-08-10 19:18:52 -0600

    Cambios To Do List
    Se listan correctamente las tareas.
    Pendiente: Agregar lógica para manejar filtrado, edición/eliminación de una tarea, check

--- frontend/src/components/ToDoList/ToDoList.jsx
+++ frontend/src/components/ToDoList/ToDoList.jsx
@@ -1,15 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { serverHost } from '../../config';
 import useFetch from '../../hooks/useFetch';
-// import Button from '../Button/Button';
+import styles from './ToDoList.module.css';
 
 function ToDoList() {
   const { callFetch, result, error, loading } = useFetch();
-  const [toDoList, setToDoList] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const fetchToDoList = () => {
     callFetch({ uri: `${serverHost}/toDoList` });
   };
+
+  const sortTasks = (tasksArray) => {
+    return tasksArray.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  };
+
+  const getDateString = (dateString) => {
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true, day: '2-digit', month: '2-digit', year: 'numeric' };
+    const date = new Date(dateString).toLocaleString('es-GT', options);
+    const [datePart, timePart] = date.split(", ");
+    return `${timePart}, ${datePart}`;
+  }
 
   useEffect(() => {
     fetchToDoList();
@@ -17,22 +28,35 @@ function ToDoList() {
 
   useEffect(() => {
     if (result) {
-      setToDoList(result);
+      setTasks(sortTasks(result));
     }
   }, [result]);
 
   return (
     <div>
-      <h1>To Do List</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
-      <ul>
-        {toDoList.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
+      <ul className={styles.todoList}>
+        {tasks.length === 0 ? (
+          <div className={styles.noTasks}>No Todo Found</div>
+        ) : (
+          tasks.map((task) => (
+            <div key={task.id} className={styles.todoItem}>
+              <input type="checkbox" className={styles.checkbox} />
+              <div className={styles.taskInfo}>
+                <span className={styles.taskTitle}>{task.title}</span>
+                <span className={styles.taskDate}>{getDateString(task.createdAt)}</span>
+              </div>
+              <div className={styles.taskActions}>
+                <button className={styles.editButton}>Edit</button>
+                <button className={styles.deleteButton}>Delete</button>
+              </div>
+            </div>
+          ))
+        )}
       </ul>
     </div>
-  )
+  );
 }
 
-export default ToDoList
+export default ToDoList;
